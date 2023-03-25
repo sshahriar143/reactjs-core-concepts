ab5b4e10330fa70dac9eee089d8eced18f475929
sshahriar143 <shimulshahriar47@gmail.com>
2023-03-26 01:36:01 +0600

    create multiple components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+const singers =[
+                {name:'Dr Mahfuz', Song:'Ay buke ay'},
+                {name:'Imran', Song:'Ay chad ay'},
+                {name:'Arijit', Song:'Ay ghure ay'},
+                {name:'Eva Rahman', Song:'Ay chole ay'},
+                {name:'Agun', Song:'Ay ghure ay'},
+  ]
 
 function App() {
+  const nayoks =['Ananta Jalil', 'Rubel', 'BappaRaz', 'Riaz', 'Jasim','Alamgir', 'Anowar']
   return (
     <div className="App">
-      <Person name='Ananta Jalil' Heroine='Borsha'></Person>
-      <Person name='Rubel' Heroine='Moushomi'></Person>
-      <Person name='Bapparaz' Heroine='Sabnur'></Person>
-      <Person name='Riaz' Heroine='Purnimaaa'></Person>
+      {
+       /*  nayoks.map(nayok => <li>{nayok}</li>) */
+      }
+      {
+        singers.map(singer => <Person name={singer.name} ></Person>)
+      }
+      {
+        /* nayoks.map(nayok => <Person name={nayok}></Person>) */
+      }
+      {/* <Person name={nayoks[0]} Heroine='Borsha'></Person>
+      <Person name={nayoks[2]} Heroine='Moushomi'></Person>
+      <Person name={nayoks[3]} Heroine='Sabnur'></Person>
+      <Person name={nayoks[1]} Heroine='Purnimaaa'></Person> */}
       <p>Bio Of Mr.Ajay......</p>
       <Friend phone='01774402393' name='Shahriar'></Friend>
       <Friend phone='01942646143' name='Shimul'></Friend>
@@ -21,7 +37,7 @@ function Person(props){
   return (
     <div className='person'>
        <h1>{props.name}</h1>
-       <p>Heroine:{props.Heroine}</p>
+       <p>{props.Heroine}</p>
     </div>
   )
   } 
